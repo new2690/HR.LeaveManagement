4ff84a7a04064d9f4ae3c8522cee8ca0aea485f4
Soroush saidy <new2690@gmail.com>
2022-09-05 15:04:41 +0430

    Add project files.

---
+++ HR.LeaveManagement.Application/Persistence/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Application.Persistence.Contracts
+{
+    public interface ILeaveTypeRepository:IGenericRepository<LeaveType>
+    {
+    }
+}
